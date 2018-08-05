40d52e938590a99be4126a61c0d9b59f94889eba
Rafal Magda <rafalmag@gmail.com>
2018-08-05 15:34:50 +0200

    pms5003 with oled display

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,85 @@
 
 // https://github.com/ThingPulse/esp8266-oled-ssd1306
 #include "SSD1306Wire.h"
-// https://github.com/beegee-tokyo/arduino-DHTesp
-#include "DHTesp.h"
+
+#include <PMS.h>
+
 // http://oleddisplay.squix.ch/
 #include "font.h"
 
-DHTesp dht;
+// violet wire 5V
+// orange wire GND
+PMS pms(Serial); // blue wire - TX (1), green wire - RX (3)
+int setPin = 16; // white wire
+
+PMS::DATA data;
+
+struct pms5003
+{
+    float pm1, pm2, pm10;
+};
+
+pms5003 pm;
+
+int readIter = 1;
+bool updatePmReads()
+{
+    int reads = 0;
+    uint16_t min1 = 0, max1 = 0, sum1 = 0,
+             min2 = 0, max2 = 0, sum2 = 0,
+             min10 = 0, max10 = 0, sum10 = 0;
+    for (int i = 0; i < readIter; i++)
+    {
+        pms.requestRead();
+        if (pms.read(data, 10000))
+        {
+            if (reads == 0)
+            {
+                min1 = data.PM_AE_UG_1_0;
+                max1 = data.PM_AE_UG_1_0;
+                sum1 = data.PM_AE_UG_1_0;
+                min2 = data.PM_AE_UG_2_5;
+                max2 = data.PM_AE_UG_2_5;
+                sum2 = data.PM_AE_UG_2_5;
+                min10 = data.PM_AE_UG_10_0;
+                max10 = data.PM_AE_UG_10_0;
+                sum10 = data.PM_AE_UG_10_0;
+            }
+            else
+            {
+                if (data.PM_AE_UG_1_0 < min1)
+                    min1 = data.PM_AE_UG_1_0;
+                if (max1 < data.PM_AE_UG_1_0)
+                    max1 = data.PM_AE_UG_1_0;
+                sum1 += data.PM_AE_UG_1_0;
+                if (data.PM_AE_UG_2_5 < min2)
+                    min2 = data.PM_AE_UG_2_5;
+                if (max2 < data.PM_AE_UG_2_5)
+                    max2 = data.PM_AE_UG_2_5;
+                sum2 += data.PM_AE_UG_2_5;
+                if (data.PM_AE_UG_10_0 < min10)
+                    min10 = data.PM_AE_UG_1_0;
+                if (max10 < data.PM_AE_UG_10_0)
+                    max10 = data.PM_AE_UG_10_0;
+                sum10 += data.PM_AE_UG_10_0;
+            }
+            reads++;
+        }
+    }
+    if (reads > 2)
+    {
+        pm.pm1 = (float)(sum1 - min1 - max1) / (float)(reads - 2);
+        pm.pm2 = (float)(sum2 - min2 - max2) / (float)(reads - 2);
+        pm.pm10 = (float)(sum10 - min10 - max10) / (float)(reads - 2);
+    }
+    else
+    {
+        pm.pm1 = min1;
+        pm.pm2 = min2;
+        pm.pm10 = min10;
+    }
+    return reads > 0;
+}
 
 // Initialize the OLED display using Wire library
 // according to http://www.instructables.com/id/ESP32-With-Integrated-OLED-WEMOSLolin-Getting-Star/
@@ -16,14 +89,24 @@ SSD1306Wire display(0x3c, 5, 4);
 
 void setup()
 {
-    Serial.begin(115200);
-    Serial.println("start");
-    Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
-
+    Serial.begin(9600);
     display.init();
-    dht.setup(16); // Connect DHT sensor to GPIO 14 (18?)
-
     display.flipScreenVertically();
+
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(Monospaced_plain_10);
+
+    display.clear();
+    display.drawString(0, 0 * 16, "Hello! Sleep 30s");
+    display.display();
+
+    digitalWrite(setPin, HIGH);
+    pms.wakeUp();
+    delay(30000);
+
+    display.clear();
+    display.drawString(0, 0 * 16, "Hello! Ready!");
+    display.display();
 }
 
 void draw()
@@ -32,22 +115,23 @@ void draw()
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.setFont(Monospaced_plain_10);
 
-    float humidity = dht.getHumidity();
-    float temperature = dht.getTemperature();
-    float heatIndex = dht.computeHeatIndex(temperature, humidity, false);
-
-    display.drawString(0, 1 * 16, String("temp    : ") + temperature + "*C");
-    display.drawString(0, 0 * 16, String("humidity: ") + humidity + "%");
-    display.drawString(0, 2 * 16, String("heat Idx: ") + heatIndex + "*C");
+    display.drawString(0, 0 * 16, String("PM 1.0: ") + pm.pm1 + "ug/m3");
+    display.drawString(0, 1 * 16, String("PM 2.5: ") + pm.pm2 + "ug/m3");
+    display.drawString(0, 2 * 16, String("PM 10 : ") + pm.pm10 + "ug/m3");
     display.drawString(0, 3 * 16, "Hello world 3");
 }
 
 void loop()
 {
-    Serial.println("next loop");
+    bool status = updatePmReads();
+    if (status)
+    {
+        display.clear();
+        draw();
+        display.display();
+    }
 
-    display.clear();
-    draw();
-    display.display();
-    delay(dht.getMinimumSamplingPeriod());
+    // pms.sleep();
+    // digitalWrite(setPin, LOW);
+    delay(5000);
 }
