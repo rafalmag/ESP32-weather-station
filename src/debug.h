463c8a1972b0169e8bb3775028c4b40c1f8422f5
Rafal Magda <rafalmag@gmail.com>
2019-02-18 21:57:06 +0100

    formatting

--- src/debug.h
+++ src/debug.h
@@ -2,56 +2,56 @@
 
 //#define SERIAL_DEBUG_ENABLED 1
 
-#define GET_NUM_ARGS(...) GET_NUM_ARGS_ACT(__VA_ARGS__, 5,4,3,2,1)
-#define GET_NUM_ARGS_ACT(_1,_2,_3,_4,_5,N,...) N
+#define GET_NUM_ARGS(...) GET_NUM_ARGS_ACT(__VA_ARGS__, 5, 4, 3, 2, 1)
+#define GET_NUM_ARGS_ACT(_1, _2, _3, _4, _5, N, ...) N
 
 #define macro_dispatcher(func, ...) \
-            macro_dispatcher_(func, GET_NUM_ARGS(__VA_ARGS__))
+      macro_dispatcher_(func, GET_NUM_ARGS(__VA_ARGS__))
 #define macro_dispatcher_(func, nargs) \
-            macro_dispatcher__(func, nargs)
+      macro_dispatcher__(func, nargs)
 #define macro_dispatcher__(func, nargs) \
-            func ## nargs
-            
+      func##nargs
+
 #ifdef SERIAL_DEBUG_ENABLED
-  #define DebugPrint(...) macro_dispatcher(DebugPrint, __VA_ARGS__)(__VA_ARGS__)
-  #define DebugPrintln(...) macro_dispatcher(DebugPrintln, __VA_ARGS__)(__VA_ARGS__)  
-  #define DebugPrint2(str,modifier)  \
-        Serial.print(millis());     \
-        Serial.print(": ");    \
-        Serial.print(__PRETTY_FUNCTION__); \
-        Serial.print(' ');      \
-        Serial.print(__LINE__);     \
-        Serial.print(' ');      \
-        Serial.print(str,modifier);
-  #define DebugPrint1(str)  \
-        Serial.print(millis());     \
-        Serial.print(": ");    \
-        Serial.print(__PRETTY_FUNCTION__); \
-        Serial.print(' ');      \
-        Serial.print(__LINE__);     \
-        Serial.print(' ');      \
-        Serial.print(str);       
-  #define DebugPrintln2(str,modifier)  \
-        Serial.print(millis());     \
-        Serial.print(": ");    \
-        Serial.print(__PRETTY_FUNCTION__); \
-        Serial.print(' ');      \
-        Serial.print(__LINE__);     \
-        Serial.print(' ');      \
-        Serial.println(str,modifier);
-  #define DebugPrintln1(str)  \
-        Serial.print(millis());     \
-        Serial.print(": ");    \
-        Serial.print(__PRETTY_FUNCTION__); \
-        Serial.print(' ');      \
-        Serial.print(__LINE__);     \
-        Serial.print(' ');      \
-        Serial.println(str);          
+#define DebugPrint(...) macro_dispatcher(DebugPrint, __VA_ARGS__)(__VA_ARGS__)
+#define DebugPrintln(...) macro_dispatcher(DebugPrintln, __VA_ARGS__)(__VA_ARGS__)
+#define DebugPrint2(str, modifier)       \
+      Serial.print(millis());            \
+      Serial.print(": ");                \
+      Serial.print(__PRETTY_FUNCTION__); \
+      Serial.print(' ');                 \
+      Serial.print(__LINE__);            \
+      Serial.print(' ');                 \
+      Serial.print(str, modifier);
+#define DebugPrint1(str)                 \
+      Serial.print(millis());            \
+      Serial.print(": ");                \
+      Serial.print(__PRETTY_FUNCTION__); \
+      Serial.print(' ');                 \
+      Serial.print(__LINE__);            \
+      Serial.print(' ');                 \
+      Serial.print(str);
+#define DebugPrintln2(str, modifier)     \
+      Serial.print(millis());            \
+      Serial.print(": ");                \
+      Serial.print(__PRETTY_FUNCTION__); \
+      Serial.print(' ');                 \
+      Serial.print(__LINE__);            \
+      Serial.print(' ');                 \
+      Serial.println(str, modifier);
+#define DebugPrintln1(str)               \
+      Serial.print(millis());            \
+      Serial.print(": ");                \
+      Serial.print(__PRETTY_FUNCTION__); \
+      Serial.print(' ');                 \
+      Serial.print(__LINE__);            \
+      Serial.print(' ');                 \
+      Serial.println(str);
 #else
-  #define DebugPrint(...) macro_dispatcher(DebugPrint, __VA_ARGS__)(__VA_ARGS__)
-  #define DebugPrintln(...) macro_dispatcher(DebugPrintln, __VA_ARGS__)(__VA_ARGS__)  
-  #define DebugPrint1(str)
-  #define DebugPrintln1(str)
-  #define DebugPrint2(str,modifier)
-  #define DebugPrintln2(str,modifier)
+#define DebugPrint(...) macro_dispatcher(DebugPrint, __VA_ARGS__)(__VA_ARGS__)
+#define DebugPrintln(...) macro_dispatcher(DebugPrintln, __VA_ARGS__)(__VA_ARGS__)
+#define DebugPrint1(str)
+#define DebugPrintln1(str)
+#define DebugPrint2(str, modifier)
+#define DebugPrintln2(str, modifier)
 #endif
